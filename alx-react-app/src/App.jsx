f101f51be3f9d70305d0866e886f271cec485d3c
mohamed elsayed <melsayed20009@gmail.com>
2025-11-02 21:08:53 +0200

    Initial Vite project upload

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,11 +2,19 @@ import WelcomeMessage from './components/WelcomeMessage.jsx';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
-import './App.css';
-
+import React from "react";
+import UserProfile from "./components/UserProfile";
 function App() {
   return (
     <>
+     <div className="App">
+      <h1>User Profile Example</h1>
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+    </div>
       <WelcomeMessage />  
       <Header />
       <MainContent />
