4611a8be9e21aee76d8c8238dce56267db301b0c
mohamed elsayed <melsayed20009@gmail.com>
2025-11-02 20:32:39 +0200

    Initial Vite project upload

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,22 +1,15 @@
 
-
-
 import Header from './Header.jsx'
-import Footer from './Footer.jsx'
 import MainContent from './MainContent.jsx'
-import WelcomeMessage from './components/WelcomeMessage.jsx'
-
+import Footer from './Footer.jsx'
 function App() {
-  
-
   return (
     <>
-    
-   <Header />
-    <MainContent />
-    <Footer />
-    <WelcomeMessage />
-    
+        
+      <Header />
+      <MainContent />
+      <Footer />
+       
     </>
   )
 }
