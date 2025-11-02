ce4c899583f0886edd4883bbe6d08b3b43ff1a3e
mohamed elsayed <melsayed20009@gmail.com>
2025-11-02 20:14:51 +0200

    Initial Vite project upload

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,21 +1,21 @@
-import WelcomeMessage from './components/WelcomeMessage.jsx'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import MainContent from './MainContent.jsx'
+import WelcomeMessage from './components/WelcomeMessage.jsx'
 
 function App() {
   
 
   return (
     <>
-    <WelcomeMessage />
+    
    <Header />
     <MainContent />
     <Footer />
+    <WelcomeMessage />
     
     </>
   )
