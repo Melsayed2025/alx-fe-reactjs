8ae2062fb4ccc7f37eebfd0e991580f988568992
mohamed elsayed <melsayed20009@gmail.com>
2025-11-06 23:25:38 +0200

    Initial Vite project upload

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -6,7 +6,7 @@ export const Counter = () => {
   return (
     <div>
       <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>decrement</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
       <button onClick={() => setCount(0)}>Reset</button>
       <p>count: {count}</p>
     </div>
