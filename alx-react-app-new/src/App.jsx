11f9dd36ae156d4ce41752059ca847685c535833
mohamed elsayed <melsayed20009@gmail.com>
2025-11-06 23:23:07 +0200

    Initial Vite project upload

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,9 +1,11 @@
+import Counter from './components/Counter.jsx';
 import WelcomeMessage from './components/WelcomeMessage.jsx';
 import Header from './components/Header.jsx';
 import MainContent from './components/MainContent.jsx';
 import Footer from './components/Footer.jsx';
 import React from "react";
 import UserProfile from "./components/UserProfile.jsx";
+
 function App() {
   return (
     <>
@@ -14,11 +16,14 @@ function App() {
         age="25" 
         bio="Loves hiking and photography" 
       />
+      
     </div>
+     <Counter /> 
       <WelcomeMessage />  
       <Header />
       <MainContent />
       <Footer />
+      
     </>
   )
 }
---
+++ alx-react-app-new/src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+      <p>count: {count}</p>
+    </div>
+  );
+};
+
+export default Counter;
