65feb90c856ab409a69b207df93e612a2c68ba1d
mohamed elsayed <melsayed20009@gmail.com>
2025-11-17 23:34:52 +0200

    Initial Vite project upload

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import AddRecipeForm from './Components/AddRecipeForm.jsx'
-import RecipeList from './Components/RecipeList.jsx'
+import AddRecipeForm from './AddRecipeForm.jsx'
+import RecipeList from './RecipeList.jsx'
 function App() {
   
 
